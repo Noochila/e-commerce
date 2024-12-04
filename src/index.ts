da61c524b41b1cfb0331b90b6b88fee1b4b913ce
noochila <manojnoochila@gmail.com>
2024-12-04 09:52:39 +0530

    updated package.json and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 const app = express()
-const port = 3000
+const port = 3000 
 
 import {rootRouter} from "./routes/index"
 
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use("/api/v1",rootRouter)
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
